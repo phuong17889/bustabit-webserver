c4b17bf19d2aacae9bb9036f1b6d41b6005cff18
Le Phuong <phuong17889@gmail.com>
2021-02-24 01:20:41 +0700

    change port

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ if(production) {
 module.exports = {
   "PRODUCTION": production,
   "DATABASE_URL": process.env.DATABASE_URL || "postgres://localhost:5432/bustabitdb",
-  "BIP32_DERIVED": process.env.BIP32_DERIVED_KEY,
+  "BIP32_DERIVED": process.env.BIP32_DERIVED_KEY || "xprv9s21ZrQH143K2QBLDr1GriAyDY2s1NRaFxMZ7asMsErVnhuiNmfM3K2X1d5VhrNdKbiQ4rsdzvg4EVwFjmBFfVeSJiqL5TWPkZwjPpPt3ak",
   "AWS_SES_KEY": process.env.AWS_SES_KEY,
   "AWS_SES_SECRET": process.env.AWS_SES_SECRET,
   "CONTACT_EMAIL": process.env.CONTACT_EMAIL || "ryan@moneypot.com",
